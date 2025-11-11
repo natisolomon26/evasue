68431c7666fcbacacd68bde2d94534b054c94316
natisolomon26 <nathadegefu@gmail.com>
2025-11-11 20:56:38 +0300

    changing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,9 @@
 import Hero from '@/components/landing/Hero';
-import { Mission } from '@/components/landing/Mission';
-import { WhatWeDo } from '@/components/landing/WhatWeDo';
-import { Campuses } from '@/components/landing/Campuses';
-import { Testimonials } from '@/components/landing/Testimonials';
-import { GetInvolved } from '@/components/landing/GetInvolved';
-import { Newsletter } from '@/components/landing/Newsletter';
-import { Values } from '@/components/landing/Values';
 
 export default function Home() {
   return (
     <div className="min-h-screen">
       <Hero />
-      <Mission />
-      <Values />
-      <WhatWeDo />
-      <Campuses />
-      <Testimonials />
-      <GetInvolved />
-      <Newsletter />
     </div>
   );
 }
