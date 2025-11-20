02628838dcc833729e59b211eecc30774025e26d
natisolomon26 <nathadegefu@gmail.com>
2025-11-20 15:42:48 +0300

    mongoose

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
-const MONGO_URL = process.env.MONGO_URL;
-if (!MONGO_URL) throw new Error("❌ Missing MONGO_URL");
-
 export const connectToDatabase = async () => {
+  const MONGO_URL = process.env.MONGO_URL;
+  if (!MONGO_URL) throw new Error("❌ Missing MONGO_URL");
+
   if (mongoose.connection.readyState === 1) return;
 
   await mongoose.connect(MONGO_URL);
